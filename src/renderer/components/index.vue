6b4e578ffc3967bbba60a0c8a5ffdb1f0c1c8745
iranphp <persianfars@gmail.com>
2019-09-23 19:51:52 +0330

    Alpha version ready

--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="content">
 
-        <div class="cloudSky"></div>
-        
-        <div v-bind:class="{slideInUp:!startClean,fadeOutUp:startClean,'delay-1s':startClean}" class="Rocket animated "></div>
+        <div class="Title">Rocket Cleaner (alpha)</div>
 
+        <div class="cloudSky"></div>
+
+        <div v-bind:class="{slideInUp:!startClean,fadeOutUp:startClean,'delay-1s':startClean}" class="Rocket animated "></div>
 
         <div v-bind:class="{fadeIn:!startClean,'delay-1s':!startClean,fadeOutUp:startClean}" class="Fire animated">
             <span class="infinite animated zoomIn"></span>
@@ -23,38 +24,133 @@
             <li class="infinite animated pulse" v-for="n in 8"></li>
         </ul>
 
+        <ul class="listlog">
+            <li class="animated fadeInUp" v-for="note in logList">{{note}}</li>
+        </ul>
+
 
     </div>
 </template>
 <script>
+
+    import util from 'util'
+    const exec = util.promisify(require('child_process').exec)
+    
     export default {
         data(){
             return{
                 startClean:false,
                 Progress:0,
+                logList:[],
+                errr:[],
             }
         },
-        watch:{
-            startClean:function (val) {
-
-                if (val==true) {
-
-                    let _this = this;
-                    let timterInterval = setInterval(function () {
-                        if (_this.Progress <= 90) {
-                            _this.Progress = _this.Progress + 10;
-                        } else {
-                            clearInterval(timterInterval);
-                            _this.Progress = 0;
-                            _this.startClean = false;
-                        }
-                    }, 600);
-                }
-            }
-        },
+        watch: {},
         methods:{
-            run(){
+            async exc(command) {
+                return exec(command)
+            },
+            async run(){
+
                 this.startClean = true;
+
+                this.Progress = 10;
+                this.logList.unshift(`Empty the Trash on all mounted volumes and the main HDD..`);
+                await this.exc(`rm -rfv /Volumes/*/.Trashes/* &>/dev/null`);
+                await this.exc(`rm -rfv ~/.Trash/* &>/dev/null`);
+
+
+
+                this.Progress = 17;
+                await this.exc(`sleep 1`);
+                this.logList.unshift(`'Clear System Log Files...'`);
+
+                try {
+                    await this.exc(`rm -rfv /private/var/log/asl/*.asl &>/dev/null`);
+                }
+                catch(error) {
+                    this.errr.push(error);
+                }
+
+
+                try {
+                    await this.exc(`rm -rfv /Library/Logs/DiagnosticReports/* &>/dev/null`);
+                }
+                catch(error) {
+                    this.errr.push(error);
+                }
+
+                try {
+                    await this.exc(`rm -rfv /Library/Logs/Adobe/* &>/dev/null`);
+                }
+                catch(error) {
+                    this.errr.push(error);
+                }
+
+
+
+
+                await this.exc(`rm -rfv ~/Library/Containers/com.apple.mail/Data/Library/Logs/Mail/* &>/dev/null`);
+                await this.exc(`rm -rfv ~/Library/Logs/CoreSimulator/* &>/dev/null`);
+
+
+                try {
+                    await this.exc(`sleep 1`);
+                    this.Progress = 24;
+                    this.logList.unshift('npm cache clean');
+                    await this.exc(`npm cache clean --force`);
+                }
+                catch(error) {
+                    this.errr.push(error);
+                }
+
+                this.Progress = 35;
+                await this.exc(`sleep 1`);
+                this.logList.unshift('Clear Adobe Cache Files...');
+                await this.exc(`rm -rfv ~/Library/Application\\ Support/Adobe/Common/Media\\ Cache\\ Files/* &>/dev/null`);
+
+                this.Progress = 40;
+                await this.exc(`sleep 1`);
+                this.logList.unshift('Cleanup iOS Applications...');
+                await this.exc(`rm -rfv ~/Music/iTunes/iTunes\\ Media/Mobile\\ Applications/* &>/dev/null`);
+
+                this.Progress = 55;
+                await this.exc(`sleep 1`);
+                this.logList.unshift('Remove iOS Device Backups...');
+                await this.exc(`rm -rfv ~/Library/Application\\ Support/MobileSync/Backup/* &>/dev/null`);
+
+                this.Progress = 64;
+                await this.exc(`sleep 1`);
+                this.logList.unshift('Cleanup XCode Derived Data and Archives...');
+                await this.exc(`rm -rfv ~/Library/Developer/Xcode/DerivedData/* &>/dev/null`);
+                await this.exc(`rm -rfv ~/Library/Developer/Xcode/Archives/* &>/dev/null`);
+
+                this.Progress = 70;
+                await this.exc(`sleep 1`);
+                this.logList.unshift('Cleanup pip cache...');
+                await this.exc(`rm -rfv ~/Library/Caches/pip`);
+
+                try {
+                    this.Progress = 87;
+                    await this.exc(`sleep 3`);
+                    this.logList.unshift('Purge inactive memory...');
+                    await this.exc(`purge`);
+                }
+                catch(error) {
+                    this.errr.push(error);
+                }
+
+                this.Progress = 100;
+                await this.exc(`sleep 2`);
+
+
+                new Notification('Successful', {
+                    body: 'The system was cleared'
+                });
+
+                this.logList = [];
+                this.startClean = false;
+
             }
         }
     }
@@ -231,6 +327,15 @@
         height: 200px;
         border-radius: 100px;
     }
+    div.Title{
+        position: absolute;
+        left: 20px;
+        top: 20px;
+        color: rgba(255,255,255,0.5);
+        font-family: Arial,serif;
+        font-weight: bold;
+        font-size: 15px;
+    }
     div.Fire{
         background-image: -moz-linear-gradient( 108deg, rgb(255,216,184) 0%, rgb(254,191,242) 100%);
         background-image: -webkit-linear-gradient( 108deg, rgb(255,216,184) 0%, rgb(254,191,242) 100%);
@@ -300,5 +405,38 @@
         font-size: 19px;
         padding-top: 38px;
         color: #fff;
+    }
+    ul.listlog{
+        width: 300px;
+        position: absolute;
+        margin: 0 auto;
+        left: 0;
+        right: 0;
+        font-family: Arial,serif;
+        font-size: 12px;
+        top: 100px;
+        max-height: 340px;
+        overflow: hidden;
+    }
+    ul.listlog li{
+        list-style: none;
+        cursor: default;
+        margin-bottom: 5px;
+        border-bottom:1px solid rgba(0,0,0,0.1);
+        color: rgba(255,255,255,0.4);
+        padding: 5px 5px 10px;
+        border-radius: 4px;
+    }
+    ul.listlog li:nth-child(1){
+        color: rgba(255,255,255,1);
+        font-weight: bold;
+    }
+    ul.listlog li:nth-child(2){
+        color: rgba(255,255,255,0.8);
+        font-weight: bold;
+    }
+    ul.listlog li:nth-child(3){
+        color: rgba(255,255,255,0.6);
+        font-weight: bold;
     }
 </style>
